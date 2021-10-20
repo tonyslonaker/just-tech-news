e8ad0a3058af1ed36dc7f9a70caa3178a4255c95
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-20 15:17:32 -0600

    update code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 const sequelize = require('./config/connection');
 
