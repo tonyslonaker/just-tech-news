7e84278059e5da106fdf7a16e89182b1870f06fa
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-21 07:48:00 -0600

    update server.js

--- javacsript/login.js
+++ javacsript/login.js
@@ -1,22 +1,48 @@
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
-
+  
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
+  
     if (username && email && password) {
-        fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        }).then((response) => {console.log(response)})
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      console.log(response);
     }
-}
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
+  }
+  Now we can add error handling by using the .ok property on the response object. You can see that at the end of the following code block:
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const hbs = exphbs.create({});
 
@@ -17,6 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
