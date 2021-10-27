d313e898b6daff79fde08a2a60177238bee88ceb
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-27 05:33:51 -0600

    update code

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
+const dashbaordRoutes = require('./dashboard-routes.js');
+
+
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
